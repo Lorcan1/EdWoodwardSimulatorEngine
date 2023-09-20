f318980f327300178d3f52920f5583e017e2430e
Lorcan1 <lorcan.rooney@ucdconnect.ie>
2023-09-20 17:16:18 +0100

    Refactored TeamSetup

--- src/main/java/com/example/matchEngine/MatchEngineBuilder/MatchEngineBuilder.java
+++
@@ -1,2 +0,0 @@
-package com.example.matchEngine.MatchEngineBuilder;public class MatchEngineBuilder {
-}
--- src/main/java/com/example/team/Team.java
+++ src/main/java/com/example/team/Team.java
@@ -12,11 +12,9 @@ import java.util.List;
 @Getter
 @Setter
 public class Team {
-    private final List<String> positions= new ArrayList<>();
-    private final String teamName;
+    private  List<String> positions= new ArrayList<>();
+    private  String teamName;
 
-    @Autowired
-    TeamSetup teamSetup;
 
     private Player gk;
     private  Player dl;
@@ -32,46 +30,5 @@ public class Team {
 
     private List<Player> players = new ArrayList<>();
 
-    @Autowired
-    public Team(String teamName) {
-        this.teamName = teamName;
-//        List<Player> team = teamSetup.returnStartingEleven(teamName);
-//            for (Player player : team) {
-//                if (player.getStartingPosition().equals("GK")) {
-//                    this.gk = player;
-//                    players.add(player);
-//                }else if(player.getStartingPosition().equals("DL")){
-//                   this.dl = player;
-//                    players.add(player);
-//                }else if(player.getStartingPosition().equals("DCL")){
-//                    this.dcl = player;
-//                    players.add(player);
-//                }else if(player.getStartingPosition().equals("DCR")){
-//                    this.dcr = player;
-//                    players.add(player);
-//                }else if(player.getStartingPosition().equals("DR")){
-//                    this.dr = player;
-//                    players.add(player);
-//                }else if(player.getStartingPosition().equals("DM")){
-//                    this.dm = player;
-//                    players.add(player);
-//                }else if(player.getStartingPosition().equals("MR")){
-//                    this.mr = player;
-//                    players.add(player);
-//                }else if(player.getStartingPosition().equals("MCR")){
-//                    this.mcr = player;
-//                    players.add(player);
-//                }else if(player.getStartingPosition().equals("MCL")){
-//                    this.mcl = player;
-//                    players.add(player);
-//                }else if(player.getStartingPosition().equals("ML")){
-//                    this.ml = player;
-//                    players.add(player);
-//                }else if(player.getStartingPosition().equals("ST")){
-//                    this.st = player;
-//                    players.add(player);
-//                }
-//            }
 
-        }
     }
